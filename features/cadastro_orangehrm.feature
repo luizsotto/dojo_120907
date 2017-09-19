23672ed99943dfd382efe24ec1e987ebde0ed61a
Luiz Cruz <luizcruz@inmetrics.com.br>
2017-09-19 14:01:15 -0300

    Atualização

--- features/cadastro_orangehrm.feature
+++ features/cadastro_orangehrm.feature
@@ -1,14 +1,16 @@
 #language: pt 
 #utf-8
 
+@cadastroorangehrm
 Funcionalidade: Fazer um Cadastro de um novo empregado
 Eu como usuario 
 Quero cadastrar um novo empregado no Site OrangeHRM
 
+
 Contexto: logar
 Dado que loguei no site OrangeHRM com usuario "Admin" e senha "admin"
 
-@cadastroorangehrm
+
 Cenario: Realizar o cadastro de um novo funcionario no site OrangeHRM
 Dado que estou na pagina Add Employee
 Quando realizar o cadastro do novo funcionario
--- features/step_definitions/cadastro_orangehrm_steps.rb
+++ features/step_definitions/cadastro_orangehrm_steps.rb
@@ -1,12 +1,21 @@
-Dado(/^que estou na pagina de cadastrar um empregado$/) do
+Dado(/^que loguei no site OrangeHRM com usuario "([^"]*)" e senha "([^"]*)"$/) do |arg1, arg2|
+  #pending # Write code here that turns the phrase above into concrete actions
+  visit 'http://opensource.demo.orangehrmlive.com/'
+  fill_in('txtUsername',:with=>arg1)
+  fill_in('txtPassword',:with=>arg2)
+  binding.pry
+  find(:xpath, '//input[@id="txtPassword"]').click
+end
+
+Dado(/^que estou na pagina Add Employee$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Quando(/^realizar o cadastro do empregado$/) do |table|
+Quando(/^realizar o cadastro do novo funcionario$/) do |table|
   # table is a Cucumber::MultilineArgument::DataTable
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Então(/^sera exibido a pagina que contem os dados pessoais do empregado$/) do
+Então(/^sera exibido a pagina que contem os dados pessoais do funcionario cadastrado$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
